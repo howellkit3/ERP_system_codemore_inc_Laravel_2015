0cee92e3e7162c30b72d24be3f7665b42d82d269
bien relampagos <bienrelampagos@gmail.com>
2015-03-19 19:09:22 +0800

    tables for sales

---
+++ mysql/status_fields.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.6.21 : Database - koufu_sale
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`koufu_sale` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `koufu_sale`;
+
+/*Table structure for table `status_fields` */
+
+DROP TABLE IF EXISTS `status_fields`;
+
+CREATE TABLE `status_fields` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `status_fields` */
+
+insert  into `status_fields`(`id`,`status`,`created_by`,`modified_by`,`created`,`modified`) values (1,'Pending',NULL,NULL,NULL,NULL),(2,'Approved',NULL,NULL,NULL,NULL),(3,'Terminate',NULL,NULL,NULL,NULL),(4,'Withdraw',NULL,NULL,NULL,NULL),(5,'Client Order',NULL,NULL,NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
