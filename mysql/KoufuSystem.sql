8e163b245364b8d9e195896d6eacec7ca02eb149
Jr <jr2wolfgang@gmail.com>
2015-04-06 19:31:53 +0800

    koufusystem db

---
+++ mysql/KoufuSystem.sql
@@ -0,0 +1,310 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.6.21 : Database - koufu_system
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`koufu_system` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `koufu_system`;
+
+/*Table structure for table `addresses` */
+
+DROP TABLE IF EXISTS `addresses`;
+
+CREATE TABLE `addresses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(45) DEFAULT NULL,
+  `foreign_key` int(11) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `address` varchar(60) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `country_id` int(11) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  `addressescol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `addresses` */
+
+/*Table structure for table `compound_substrates` */
+
+DROP TABLE IF EXISTS `compound_substrates`;
+
+CREATE TABLE `compound_substrates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  `name` varchar(80) DEFAULT NULL,
+  `manufacturer_id` int(11) DEFAULT NULL,
+  `layers` smallint(6) DEFAULT NULL,
+  `remarks` text,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `compound_substrates` */
+
+/*Table structure for table `corrugated_papers` */
+
+DROP TABLE IF EXISTS `corrugated_papers`;
+
+CREATE TABLE `corrugated_papers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  `name` varchar(120) DEFAULT NULL,
+  `manufacturer_id` int(11) DEFAULT NULL,
+  `layers` smallint(6) DEFAULT NULL,
+  `brust` int(11) DEFAULT NULL,
+  `fct` int(11) DEFAULT NULL,
+  `remark` text,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `corrugated_papers` */
+
+/*Table structure for table `general_items` */
+
+DROP TABLE IF EXISTS `general_items`;
+
+CREATE TABLE `general_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  `name` varchar(120) DEFAULT NULL,
+  `manufacturer_id` int(11) DEFAULT NULL,
+  `measure` varchar(45) DEFAULT NULL,
+  `remarks` text,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `general_items` */
+
+/*Table structure for table `item_category_holders` */
+
+DROP TABLE IF EXISTS `item_category_holders`;
+
+CREATE TABLE `item_category_holders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `item_category_holders` */
+
+/*Table structure for table `item_group_layers` */
+
+DROP TABLE IF EXISTS `item_group_layers`;
+
+CREATE TABLE `item_group_layers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(45) DEFAULT NULL,
+  `foreign_key` int(11) DEFAULT NULL,
+  `no` smallint(6) DEFAULT NULL,
+  `flute` varchar(80) DEFAULT NULL,
+  `substrate` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `item_group_layers` */
+
+/*Table structure for table `item_type_holders` */
+
+DROP TABLE IF EXISTS `item_type_holders`;
+
+CREATE TABLE `item_type_holders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_category_holder_id` int(11) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `item_type_holders` */
+
+/*Table structure for table `packaging_holders` */
+
+DROP TABLE IF EXISTS `packaging_holders`;
+
+CREATE TABLE `packaging_holders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `packaging_holders` */
+
+/*Table structure for table `payment_term_holders` */
+
+DROP TABLE IF EXISTS `payment_term_holders`;
+
+CREATE TABLE `payment_term_holders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `payment_term_holders` */
+
+/*Table structure for table `phones` */
+
+DROP TABLE IF EXISTS `phones`;
+
+CREATE TABLE `phones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(45) DEFAULT NULL,
+  `foreign_key` int(11) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `number` varchar(45) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `phones` */
+
+/*Table structure for table `status_field_holders` */
+
+DROP TABLE IF EXISTS `status_field_holders`;
+
+CREATE TABLE `status_field_holders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `status_field_holders` */
+
+/*Table structure for table `substrates` */
+
+DROP TABLE IF EXISTS `substrates`;
+
+CREATE TABLE `substrates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  `name` varchar(120) DEFAULT NULL,
+  `manufacturer_id` int(11) DEFAULT NULL,
+  `type` varchar(80) DEFAULT NULL,
+  `thickness` varchar(80) DEFAULT NULL,
+  `remarks` text,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `substrates` */
+
+/*Table structure for table `suppliers` */
+
+DROP TABLE IF EXISTS `suppliers`;
+
+CREATE TABLE `suppliers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` int(11) DEFAULT NULL,
+  `name` varchar(180) DEFAULT NULL,
+  `description` text,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `suppliers` */
+
+/*Table structure for table `units` */
+
+DROP TABLE IF EXISTS `units`;
+
+CREATE TABLE `units` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `unit` varchar(60) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `units` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` int(11) NOT NULL,
+  `first_name` varchar(120) NOT NULL,
+  `last_name` varchar(120) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `rxt` varchar(60) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`uuid`,`first_name`,`last_name`,`email`,`username`,`password`,`rxt`,`created_by`,`updated_by`,`created`,`modified`) values (1,0,'bien','relampagos','bien@yahoo.com','','434279e1ad1ee629abd7e12b74b50082d4bf4ccd','12345678',NULL,NULL,'2015-03-30 00:02:17','2015-03-30 00:02:17');
+insert  into `users`(`id`,`uuid`,`first_name`,`last_name`,`email`,`username`,`password`,`rxt`,`created_by`,`updated_by`,`created`,`modified`) values (2,0,'Jr','Relampagos','jr2wolfgang@gmail.com','','4f764ee89b21700f45820b3d271040322a6b084a','metallica',NULL,NULL,'2015-04-06 09:21:52','2015-04-06 09:21:52');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
