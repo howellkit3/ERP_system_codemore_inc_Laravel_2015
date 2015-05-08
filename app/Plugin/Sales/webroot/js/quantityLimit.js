f17de4ec03af39f5b212a3fffe891f05c42717a0
bien relampagos <bienrelampagos@gmail.com>
2015-05-08 08:35:55 +0800

    js for quantity limit

--- app/Plugin/Sales/webroot/js/quantityLimit.js
+++ app/Plugin/Sales/webroot/js/quantityLimit.js
@@ -1,6 +1,39 @@
 $( document ).ready(function() {
 
-	$("body").on('onkeyup','.quantityLimit', function(e){
-	 // stuff happens
-	});
+		$("body").on('keyup','.quantityLimit', function(e){
+
+			var quantityValue = $('#quantity').val();
+
+			var fields = $('.quantityLimit');
+			var total = '';
+			var limit = '';
+			     
+		    $(fields).each(function() {
+		     
+		        var num = parseInt(this.value);
+		          
+				if (!isNaN(this.value)) {
+
+					total = Number(total) + num;
+
+				}
+		         
+				if ( total > quantityValue ){
+					$('.add-field').hide();
+					console.log(total);
+					alert('Max Quantity');
+					exceed = total - quantityValue;
+
+					console.log('max reached');
+
+				} else {
+
+					console.log(total);
+					$('.add-field').show();
+					console.log('max not reached');
+
+				}
+
+			});
+		});
 });
