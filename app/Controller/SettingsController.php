ad933bb676d8d8184ecda6b59e85d0474c6a417d
bien relampagos <bienrelampagos@gmail.com>
2015-05-13 12:01:07 +0800

    bugs

--- app/Controller/SettingsController.php
+++ app/Controller/SettingsController.php
@@ -1864,7 +1864,7 @@ class SettingsController extends AppController
                 //pr($this->request->data);exit();
                 $this->User->id = $this->request->data['User']['id'];
                 $this->User->saveField('role_id', $this->request->data['Role']['id']);
-                $this->Session->setFlash(__('Permission Successfully deleted..'));
+                $this->Session->setFlash(__('Permission Successfully added..'));
 
                 $this->redirect(
                     array('controller' => 'settings', 'action' => 'role_perm')
