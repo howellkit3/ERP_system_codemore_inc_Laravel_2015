e03aba481891d5da6d77613f6fdf15e6858e8fde
Howell Kit Calabia <howellkitacalabia@gmail.com>
2015-05-21 11:44:22 +0800

    make corrugated papers > edit organized the as compound substrate > edit .

--- app/webroot/js/EditLayerCorrugatedPaper.js
+++ app/webroot/js/EditLayerCorrugatedPaper.js
@@ -10,13 +10,6 @@ $('#CorrugatedPaperLayers').blur(function(){
 	var count = 1;
 	for (i = 0; i < selected; i++) { 
 
-		if (count > 1){		
-
-			$inputs += '<div class="form-group corrugatedPaper-layers"><label class="col-lg-2 control-label"></label>';
-			$inputs += '<div class="col-lg-8"><hr style="color:#99CC99"></div></div>';
-
-		}
-
 		$inputs += '<div class="form-group corrugatedPaper-layers"><label class="col-lg-3 control-label"><span style="color:red">*</span>Substrate  '+count+'</label>';
 		$inputs += '<div class="col-lg-8"><input type="hidden" maxlength="120"  class="form-control layer" name="data[ItemGroupLayer][no][]" value="' + i + '"></div>';
 		$inputs += '<div class="col-lg-7"><input type="text" maxlength="120" required = "required"  placeholder = "Substrate name" class="form-control layer" name="data[ItemGroupLayer][substrate][]"></div></div>';
@@ -24,6 +17,9 @@ $('#CorrugatedPaperLayers').blur(function(){
 		$inputs += '<div class="form-group corrugatedPaper-layers"><label class="col-lg-3 control-label"><span style="color:red">*</span>Flute  '+count+'</label>';
 		$inputs += '<div class="col-lg-7"><input type="text" maxlength="120"  placeholder = "Flute" class="form-control layer" name="data[ItemGroupLayer][flute][]"></div></div>';
 		count++;
+
+		$inputs += '<div class="form-group corrugatedPaper-layers"><label class="col-lg-2 control-label"></label>';
+			$inputs += '<div class="col-lg-8"><hr style="color:#99CC99"></div></div>';
 	}
 
 
