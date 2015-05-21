263bd895547e5f58744bdd4a2f018c124ef069d2
bien relampagos <bienrelampagos@gmail.com>
2015-05-21 08:38:15 +0800

    utf8

--- app/Plugin/Sales/webroot/js/draggableproducts.js
+++ app/Plugin/Sales/webroot/js/draggableproducts.js
@@ -562,3 +562,13 @@ $(document).ready(function() {
 
 });
 
+// function encode_utf8(s) { 
+
+//  return unescape(encodeURIComponent(s)); 
+
+// }
+// function decode_utf8(s) { 
+
+//     return decodeURIComponent(escape(s)); 
+
+// }
